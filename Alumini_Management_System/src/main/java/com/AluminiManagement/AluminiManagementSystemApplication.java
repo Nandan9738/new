9b088d4a2b06f84b4ed4f8ae69bf93c600162f54
nandan <nandancena24@gmail.com>
2023-03-23 21:41:19 +0530

    comitted

---
+++ Alumini_Management_System/src/main/java/com/AluminiManagement/AluminiManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.AluminiManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluminiManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AluminiManagementSystemApplication.class, args);
+	}
+
+}
